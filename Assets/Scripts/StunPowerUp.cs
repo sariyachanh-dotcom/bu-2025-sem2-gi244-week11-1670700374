5ac5e6fe5af4078083ea0acc275f80cd64657b93
sariyachanh-dotcom <sariya.chanh@bumail.net>
2026-03-30 07:09:14 +0700

    Current Scripts StunPowerUp.cs

---
+++ Assets/Scripts/StunPowerUp.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class StunPowerUp : MonoBehaviour
+{
+    public float stunDuration = 5f;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+            foreach (Enemy e in enemies)
+            {
+                e.Stun(stunDuration);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
