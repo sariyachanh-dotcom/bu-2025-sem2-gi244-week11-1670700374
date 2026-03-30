9cfd47bd6757b57302e18b288893d51efdc27232
sariyachanh-dotcom <sariya.chanh@bumail.net>
2026-03-30 07:02:00 +0700

    Current Scripts Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -6,14 +7,41 @@ public class Enemy : MonoBehaviour
     private Rigidbody rb;
     private GameObject player;
 
+    private bool isStunned = false;
+    private float stunTimer = 0;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isStunned)//stuned
+        {
+            stunTimer -= Time.deltaTime;
+            if (stunTimer <= 0)
+            {
+                isStunned = false;
+            }
+            return;
+        }
 
+        
+        if (player != null)//go to the player
+        {
+            Vector3 dir = (player.transform.position - transform.position).normalized;
+            rb.MovePosition(transform.position + dir * speed * Time.deltaTime);
+        }
+    }
+
+    public void Stun(float duration)
+    {
+        isStunned = true;
+        stunTimer = duration;
     }
 }
+
+
